8f582b556d31340a2fd41f24af556f731d05d979
KallDrexx <me@mshapiro.net>
2019-09-14 09:46:18 -0400

    Created monogame application with imgui support

---
+++ MazePlayground.App.MonoGame/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MazePlayground.App.MonoGame
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            using (var game = new App())
+            {
+                game.Run();
+            }
+        }
+    }
+}
